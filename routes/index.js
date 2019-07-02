c6c08c3af92959a0a5ec7df2f3c1430c76d596c6
Cecile Pham <sheshiru.van@gmail.com>
2019-07-02 11:37:38 +0200

    started home page css

--- routes/index.js
+++ routes/index.js
@@ -2,28 +2,32 @@ const express = require("express");
 const router = new express.Router();
 
 router.get(["/", "/home"], (req, res) => {
-  res.render("home");
+  let bigWrapper = "bg-home";
+  let navbar = "navbar-home";
+  let mainTitle = "midi";
+  res.render("home", { bigWrapper, navbar, mainTitle });
 });
 
 router.get("/restaurants", (req, res) => {
-  res.render("restaurants");
-})
+  let bigWrapper = "bg-rest";
+  let navbar = "navbar-rest";
+  res.render("restaurants", { bigWrapper, navbar });
+});
 
 router.get("/account", (req, res) => {
   res.render("user-account");
-})
+});
 
 router.get("/random", (req, res) => {
   res.render("random-restaurant");
-})
+});
 
 router.get("/wishlist", (req, res) => {
   res.render("wishlist");
-})
+});
 
 router.get("/favorites", (req, res) => {
   res.render("favorites");
-})
-
+});
 
 module.exports = router;
