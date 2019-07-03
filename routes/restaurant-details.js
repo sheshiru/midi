1c554ffc3e967283bc902c5c43811cb89f15ff64
leaandrea <leaandreapaumier@gmail.com>
2019-07-03 11:24:33 +0200

    test css

--- routes/restaurant-details.js
+++ routes/restaurant-details.js
@@ -5,8 +5,8 @@ const Restaurant = require("../models/Restaurant");
 router.get("/restaurant/:id", (req, res) => {
   Restaurant.findById(req.params.id)
     .then(resto => {
-      let likesCount = resto.favorites.length
-      res.render("restaurant-details", { resto, likesCount });
+      let likesCount = resto.favorites.length;
+      res.render("restaurant-details", { resto, likesCount, navlayout: true });
     })
     .catch(err => {
       console.log(err);
