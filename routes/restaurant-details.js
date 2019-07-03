34c1b1008fbcc2368bcc61fa1e104e747a2658e4
leaandrea <leaandreapaumier@gmail.com>
2019-07-03 11:37:03 +0200

    abcd

--- routes/restaurant-details.js
+++ routes/restaurant-details.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const router = new express.Router();
 const Restaurant = require("../models/Restaurant");
+const distance = require("google-distance-matrix");
+const getDistance = require("./google_distance");
+const comp = require("../models/Company");
 
-router.get("/restaurant/:id", (req, res) => {
-  Restaurant.findById(req.params.id)
-    .then(resto => {
-      let likesCount = resto.favorites.length;
-      res.render("restaurant-details", { resto, likesCount, navlayout: true });
-    })
-    .catch(err => {
-      console.log(err);
+router.get("/restaurant/:id", async (req, res) => {
+  try {
+    const resto = await Restaurant.findById(req.params.id);
+    const company = await comp.findById(req.query.companyId);
+    getDistance([resto.address], [company.address], distance => {
+      res.render("restaurant-details", {
+        resto,
+        company,
+        distance,
+        navlayout: true
+      });
     });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
