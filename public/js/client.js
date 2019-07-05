498228a1beac99f5285b5ab02ac8a5a59b47955d
leaandrea <leaandreapaumier@gmail.com>
2019-07-05 11:05:02 +0200

    fixed affichage speed

--- public/js/client.js
+++ public/js/client.js
@@ -44,18 +44,18 @@ function displayRestos(restos) {
 );
 //
 
-// button.addEventListener("click", function(e) {
-//   e.preventDefault();
-//   const resto_id = document
-//     .querySelector(".speedContainerButtons")
-//     .getAttribute("data-id");
-//   axios
-//     .post(`${siteUrl}/api/restaurant/${resto_id}`, {
-//       speed: document.querySelector("[name=speed]:checked").value
-//     })
-//     .then(dbRes => console.log(dbRes))
-//     .catch(dbErr => console.log(dbErr));
-// });
+button.addEventListener("click", function(e) {
+  e.preventDefault();
+  const resto_id = document
+    .querySelector(".speedContainerButtons")
+    .getAttribute("data-id");
+  axios
+    .post(`${siteUrl}/api/restaurant/${resto_id}`, {
+      speed: document.querySelector("[name=speed]:checked").value
+    })
+    .then(dbRes => console.log(dbRes))
+    .catch(dbErr => console.log(dbErr));
+});
 
 input.onkeyup = evt => filterRestos(evt.target.value);
 
--- routes/restaurant-details.js
+++ routes/restaurant-details.js
@@ -13,14 +13,29 @@ router.get("/restaurant/:id", guardRoute, async (req, res) => {
     const company = await comp.findById(req.query.companyId);
     let speed;
     getDistance([resto.address], [company.address], distance => {
-      if (resto.speed.quick >= resto.speed.slow && resto.speed.medium) {
-        speed = "quick";
+      if (
+        resto.speed.quick >= resto.speed.slow &&
+        resto.speed.quick >= resto.speed.medium
+      ) {
+        speed = "fast";
       }
-      if (resto.speed.slow >= resto.speed.quick && resto.speed.medium) {
+      if (
+        resto.speed.slow >= resto.speed.quick &&
+        resto.speed.slow >= resto.speed.medium
+      ) {
         speed = "slow";
       }
-      if (resto.speed.medium >= resto.speed.slow && resto.speed.quick) {
-        speed = "medium";
+      if (
+        resto.speed.medium >= resto.speed.slow &&
+        resto.speed.medium >= resto.speed.quick
+      ) {
+        speed = "normal";
+      }
+      if (
+        resto.speed.medium == resto.speed.quick &&
+        resto.speed.medium == resto.speed.slow
+      ) {
+        speed = "normal";
       }
       res.render("restaurant-details", {
         resto,
