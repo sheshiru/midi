abc45726ef86f22f1d4bd01d47a2dd7b40005c6d
Camille Jacques <camille.jacques75@gmail.com>
2019-07-05 14:46:44 +0200

    new seeds and cuisine types

--- bin/seeds.js
+++ bin/seeds.js
@@ -6,7 +6,7 @@ const restaurants = [
     name: "Bao Bao",
     typeOfCuisine: ["Chinese"],
     address: "4 rue Alexandre Dumas Paris",
-    speed: "Medium",
+    speed: { quick: 0, medium: 6, slow: 0 },
     image:
       "https://cdn1.centralapp.com/api/v1/media/gallery-large/place-5753-t3ecunt851une4s34lbz.jpeg",
     takeout: "Both",
@@ -17,7 +17,7 @@ const restaurants = [
     name: "Le Comptoir du Poulet",
     typeOfCuisine: ["Chicken"],
     address: "3 rue de Lagny Paris",
-    speed: "Medium",
+    speed: { quick: 4, medium: 0, slow: 0 },
     image: "https://f.roocdn.com/images/menus/96486/header-image.jpg",
     takeout: "Both",
     recommendations: ["1/2 poulet"],
@@ -27,9 +27,42 @@ const restaurants = [
     name: "Mme Shawn",
     typeOfCuisine: ["Thai"],
     address: "23 rue Paul Bert Paris",
-    speed: "Medium",
+    speed: { quick: 0, medium: 6, slow: 0 },
     image:
       "https://www.sortiraparis.com/images/55/14066/259051-mme-shawn-bistrot-thailandais-arrive-chez-ubereats.jpg",
+    takeout: "Both",
+    recommendations: [],
+    favorites: []
+  },
+  {
+    name: "Café Titon",
+    typeOfCuisine: ["German"],
+    address: "34 Rue Titon 75011 Paris",
+    speed: { quick: 3, medium: 0, slow: 0 },
+    image:
+      "https://u.tfstatic.com/restaurant_photos/933/210933/169/612/cafe-titon-vue-detail-table-a1f7c.jpg",
+    takeout: "Both",
+    recommendations: [],
+    favorites: []
+  },
+  {
+    name: "Café Moco",
+    typeOfCuisine: ["French"],
+    address: "177bis Boulevard Voltaire, 75011 Paris",
+    speed: { quick: 0, medium: 0, slow: 3 },
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/R1P5MELboIx63B4cp5PcXA/ls.jpg",
+    takeout: "Both",
+    recommendations: [],
+    favorites: []
+  },
+  {
+    name: "Gharbia Bio",
+    typeOfCuisine: ["Egyptian"],
+    address: "174 Boulevard Voltaire, 75011 Paris",
+    speed: { quick: 5, medium: 0, slow: 0 },
+    image:
+      "https://uploads.lebonbon.fr/source/2018/paris/juin/mel/Gharbia2.jpg",
     takeout: "Both",
     recommendations: [],
     favorites: []
